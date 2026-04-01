96be7209e7af30a6a72e71ea71dcfd890f80d6ad
Jaymes <jaylink5353@gmail.com>
2026-04-01 12:35:16 -0400

    Got a basic m3u8 writer, going to make path converter now

---
+++ M3U8LibCLI/M3U8LibCLI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+
+namespace m3u8Manager
+{
+    internal class Program
+    {
+        public static List<string> SongDatabase = new List<string>();
+        static void Main(string[] args)
+        {
+            SongDatabase.Add("/Music/Music/Music/song!");
+            SongDatabase.Add("musicPath2?");
+            checkFile(@"C:\Users\Jaymes\Desktop\testPlaylist.m3u8");
+        }
+
+        static public void checkFile(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.Write("#EXTMCU");
+
+                foreach (var result in SongDatabase)
+                {
+                    sw.Write("\n");
+
+                    sw.WriteLine(result);
+                }
+            }
+        }
+    }
+}
